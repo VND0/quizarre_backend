d39b06c49523a10c7e21f1270a96037885db9a45
user <varlamov577123@gmail.com>
2026-04-19 00:53:38 +0300

    Added changing profile info with PATCH request

--- app/api/users.py
+++ app/api/users.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import select
 
 from ..core.models import JwtPayload
-from ..core.security import verify_jwt
+from ..core.security import verify_jwt, get_password_hash
 from ..db.db import SessionDep
-from ..models.user import User
+from ..models.user import User, UserPatch
 
 router = APIRouter(
     prefix="/api/users",
@@ -15,9 +15,39 @@ router = APIRouter(
 )
 
 
-@router.get("/me", response_model=User)
+@router.get("/me", response_model=User, )
 def get_own_info(
         session: SessionDep,
         jwt_payload: JwtPayload = Depends(verify_jwt)
 ):
     return session.exec(select(User).where(User.id == jwt_payload.sub)).one()
+
+
+@router.patch(
+    "/me",
+    response_model=User,
+    responses={
+        409: {"description": "New email is busy"},
+    }
+)
+def edit_user_info(
+        session: SessionDep,
+        update_data: UserPatch,
+        jwt_payload: JwtPayload = Depends(verify_jwt)
+):
+    user = session.exec(select(User).where(User.id == jwt_payload.sub)).one()
+
+    if update_data.email is not None and update_data.email != user.email:
+        user_with_new_email = session.exec(select(User).where(User.email == update_data.email)).one_or_none()
+        if user_with_new_email:
+            raise HTTPException(409, "New email is busy")
+
+    if update_data.password is not None:
+        new_password_hash = get_password_hash(update_data.password)
+        update_data.password = None
+        user.password_hash = new_password_hash
+
+    user.sqlmodel_update(update_data.model_dump(exclude_none=True))
+    session.commit()
+    session.refresh(user)
+    return user
--- app/models/user.py
+++ app/models/user.py
@@ -1,14 +1,24 @@
 import datetime
 import uuid
 from string import ascii_uppercase, ascii_lowercase
+from typing import Annotated
 from uuid import uuid4, UUID
 
-from pydantic import EmailStr, field_validator
+from pydantic import EmailStr, AfterValidator
 from sqlmodel import SQLModel, Field
 
 DIGITS = set("0123456789")
 UPPERCASE_LETTERS = set(ascii_uppercase)
 LOWERCASE_LETTERS = set(ascii_lowercase)
+
+
+def validate_password(value: str):
+    chars = set(value)
+    if not chars & DIGITS:
+        raise ValueError("Password must contain digits")
+    if not chars & LOWERCASE_LETTERS:
+        raise ValueError("Password must contain lowercase latin letters")
+    return value
 
 
 class BaseUser(SQLModel):
@@ -17,17 +27,7 @@ class BaseUser(SQLModel):
 
 
 class NewUser(BaseUser):
-    password: str = Field(min_length=8, max_length=64)
-
-    @field_validator("password")
-    @classmethod
-    def validate_password(cls, value: str):
-        chars = set(value)
-        if not chars & DIGITS:
-            raise ValueError("Password must contain digits")
-        if not chars & LOWERCASE_LETTERS:
-            raise ValueError("Password must contain lowercase latin letters")
-        return value
+    password: Annotated[str, AfterValidator(validate_password)] = Field(min_length=8, max_length=64)
 
 
 class User(BaseUser, table=True):
@@ -55,6 +55,13 @@ class NewTokens(SQLModel):
 class UserResponse(SQLModel):
     user: User
     tokens: NewTokens
+
+
+class UserPatch(SQLModel):
+    email: EmailStr | None = Field(max_length=254, default=None)
+    name: str | None = Field(min_length=3, max_length=72, default=None)
+    password: Annotated[str, AfterValidator(validate_password)] | None = Field(min_length=8, max_length=64,
+                                                                               default=None)
 
 
 class ExistingRefreshToken(SQLModel):
