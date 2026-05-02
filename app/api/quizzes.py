8a732086240a43a00b5fc14b6a1bfed24f2aa6bf
user <varlamov577123@gmail.com>
2026-05-02 22:24:23 +0300

    Implemented some endpoints for quizzes

--- app/api/quizzes.py
+++ app/api/quizzes.py
@@ -1,15 +1,41 @@
-from fastapi import APIRouter, Depends
+import uuid
+
+from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import select
 
 from ..core.models import JwtPayload
 from ..core.security import verify_jwt
 from ..db.db import SessionDep
-from ..models.quizzes import Quiz, FullQuizResponse
+from ..models.quizzes import Quiz, FullQuizResponse, QuestionResponse, BaseTestAnswer, BaseTextAnswer, QuizUpload, \
+    TestAnswer, TextAnswer, Question
 
 router = APIRouter(
     prefix="/api/quizzes",
     tags=["Quizzes"]
 )
+
+
+def prepare_quiz_response(quiz: Quiz) -> FullQuizResponse:
+    questions = []
+    for q in quiz.questions:
+        test_answers = []
+        for ta in q.test_answers:
+            test_answers.append(BaseTestAnswer(**ta.model_dump(by_alias=True)))
+
+        text_answer = []
+        for ta in q.text_answers:
+            text_answer.append(BaseTextAnswer(**ta.model_dump(by_alias=True)))
+
+        questions.append(QuestionResponse.model_validate({
+            **q.model_dump(exclude={"test_answers", "text_answers"}, by_alias=True),
+            "testAnswers": [ta.model_dump(by_alias=True) for ta in test_answers],
+            "textAnswers": [ta.model_dump(by_alias=True) for ta in text_answer],
+        }))
+
+    return FullQuizResponse.model_validate({
+        **quiz.model_dump(by_alias=True),
+        "questions": [q.model_dump(by_alias=True) for q in questions]
+    })
 
 
 @router.get("", response_model=list[Quiz])
@@ -23,8 +49,45 @@ async def get_quizzes(
 @router.get("/{quiz_id}", response_model=FullQuizResponse)
 async def get_quiz_data(
         session: SessionDep,
-        quiz_id: str,
+        quiz_id: uuid.UUID,
         jwt_payload: JwtPayload = Depends(verify_jwt),
 ):
-    quiz = session.exec(select(Quiz).where(Quiz.id == quiz_id)).one_or_none()
-    if quiz is None
+    quiz: Quiz | None = session.exec(select(Quiz).where(Quiz.id == quiz_id)).one_or_none()
+    if quiz is None:
+        raise HTTPException(404)
+    if quiz.user_id != jwt_payload.sub:
+        raise HTTPException(403)
+
+    return prepare_quiz_response(quiz)
+
+
+@router.post("", response_model=FullQuizResponse)
+async def upload_quiz(
+        session: SessionDep,
+        quiz_upload: QuizUpload,
+        jwt_payload: JwtPayload = Depends(verify_jwt),
+):
+    questions = []
+    for q in quiz_upload.questions:
+        test_answers = []
+        text_answers = []
+
+        for ta in q.test_answers:
+            test_answers.append(TestAnswer(**ta.model_dump()))
+        for ta in q.text_answers:
+            text_answers.append(TextAnswer(**ta.model_dump()))
+
+        questions.append(Question(
+            **q.model_dump(exclude={"test_answers", "text_answers"}),
+            test_answers=test_answers,
+            text_answers=text_answers,
+        ))
+
+    quiz = Quiz(
+        **quiz_upload.model_dump(exclude={"questions"}),
+        user_id=jwt_payload.sub,
+        questions=questions
+    )
+    session.add(quiz)
+    session.commit()
+    return prepare_quiz_response(quiz)
--- app/models/quizzes.py
+++ app/models/quizzes.py
@@ -14,17 +14,23 @@ class QuestionTypes(Enum):
     TEXT = "text"
 
 
-class BaseQuiz(SQLModel):
-    id: UUID = Field(default_factory=uuid4, primary_key=True)
+class QuizData(SQLModel):
     title: str = Field(min_length=3, max_length=150)
     description: str = Field(min_length=3, max_length=2000)
 
-    time_to_answer: int = Field(ge=1, le=31536000, alias="timeToAnswer")  # 31536000 is 365 days in seconds
-    show_answer_immediately: bool = Field(alias="showAnswerImmediately")
-    require_fullscreen: bool = Field(alias="requireFullscreen")
+    time_to_answer: int = Field(ge=1, le=31536000, alias="timeToAnswer",  # 31536000 is 365 days in seconds
+                                description="Time to answer each question in seconds")
+    show_answer_immediately: bool = Field(alias="showAnswerImmediately",
+                                          description="If true, user will see the correct option right after he answered.")
+    require_fullscreen: bool = Field(alias="requireFullscreen",
+                                     description="If true, user must enter fullscreen mode to play")
     shuffle_questions: bool = Field(alias="shuffleQuestions")
-    encourage_speed: bool = Field(alias="encourageSpeed")
+    encourage_speed: bool = Field(alias="encourageSpeed",
+                                  description="If true, user will have higher scores for the answers (up to 1.5x).")
 
+
+class BaseQuiz(QuizData):
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
     user_id: UUID | None = Field(default=None, foreign_key="user.id", ondelete="CASCADE", alias="userId")
 
 
@@ -33,14 +39,16 @@ class Quiz(BaseQuiz, table=True):
     questions: list["Question"] = Relationship(back_populates="quiz")
 
 
-class BaseQuestion(SQLModel):
-    id: UUID = Field(default_factory=uuid4, primary_key=True)
+class QuestionData(SQLModel):
     text: str = Field(min_length=3, max_length=3000)
     description: str | None = Field(min_length=3, max_length=5000, default=None)
     type: QuestionTypes
-    order_index: int = Field(le=0, alias="orderIndex")
-    points: float = Field(le=0)
+    order_index: int = Field(ge=0, alias="orderIndex")
+    points: float = Field(ge=0)
 
+
+class BaseQuestion(QuestionData):
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
     quiz_id: UUID | None = Field(default=None, foreign_key="quiz.id", ondelete="CASCADE", alias="quizId")
 
 
@@ -51,12 +59,14 @@ class Question(BaseQuestion, table=True):
     text_answers: list["TextAnswer"] = Relationship(back_populates="question")
 
 
-class BaseTestAnswer(SQLModel):
-    id: UUID = Field(default_factory=uuid4, primary_key=True)
+class TestAnswerData(SQLModel):
     text: str = Field(min_length=1, max_length=255)
-    order_index: int = Field(le=0, alias="orderIndex")
+    order_index: int = Field(ge=0, alias="orderIndex")
     is_correct: bool = Field(alias="isCorrect")
 
+
+class BaseTestAnswer(TestAnswerData):
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
     question_id: UUID | None = Field(default=None, foreign_key="question.id", ondelete="CASCADE", alias="questionId")
 
 
@@ -64,10 +74,12 @@ class TestAnswer(BaseTestAnswer, table=True):
     question: Question = Relationship(back_populates="test_answers")
 
 
-class BaseTextAnswer(SQLModel):
-    id: UUID = Field(default_factory=uuid4, primary_key=True)
+class TextAnswerData(SQLModel):
     text: str = Field(min_length=1, max_length=3000)
 
+
+class BaseTextAnswer(TextAnswerData):
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
     question_id: UUID | None = Field(default=None, foreign_key="question.id", ondelete="CASCADE", alias="questionId")
 
 
@@ -82,3 +94,12 @@ class QuestionResponse(BaseQuestion):
 
 class FullQuizResponse(BaseQuiz):
     questions: list[QuestionResponse]
+
+
+class QuestionUpload(QuestionData):
+    test_answers: list[TestAnswerData] = Field(alias="testAnswers")
+    text_answers: list[TextAnswerData] = Field(alias="textAnswers")
+
+
+class QuizUpload(QuizData):
+    questions: list[QuestionUpload]
