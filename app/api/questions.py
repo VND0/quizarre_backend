3f5a1b86a63ac7be7db79a354e58d1ca2d096e8e
user <varlamov577123@gmail.com>
2026-05-06 20:26:05 +0300

    Implemented endpoint to get question by id

--- app/api/questions.py
+++ app/api/questions.py
@@ -6,7 +6,7 @@ from sqlmodel import select
 from ..core.models import JwtPayload
 from ..core.security import verify_jwt
 from ..db.db import SessionDep
-from ..models.questions import Question
+from ..models.questions import Question, QuestionResponse
 from ..models.quizzes import Quiz
 
 router = APIRouter(
@@ -38,3 +38,32 @@ async def get_questions(
         raise HTTPException(403)
 
     return quiz.questions
+
+
+def prepare_question_response(question: Question) -> QuestionResponse:
+    test_answers = [ta.model_dump(by_alias=True) for ta in question.test_answers]
+    text_answers = [ta.model_dump(by_alias=True) for ta in question.text_answers]
+
+    return QuestionResponse.model_validate({
+        **question.model_dump(by_alias=True),
+        "testAnswers": test_answers,
+        "textAnswers": text_answers,
+    })
+
+
+@router.get(
+    "/{question_id}",
+    response_model=QuestionResponse
+)
+async def get_question(
+        question_id: uuid.UUID,
+        session: SessionDep,
+        jwt_payload: JwtPayload = Depends(verify_jwt),
+):
+    question: Question | None = session.exec(select(Question).where(Question.id == question_id)).one_or_none()
+    if question is None:
+        raise HTTPException(404)
+    if question.quiz.user_id != jwt_payload.sub:
+        raise HTTPException(403)
+
+    return prepare_question_response(question)
